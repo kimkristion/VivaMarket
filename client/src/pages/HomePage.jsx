57c5f746a28293cb0331f838e2c669bf8de519fa
Alexis Lopez <lopez111alex@gmail.com>
2024-01-10 15:16:01 -0800

    final commit

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 export default function HomePage() {
   return (
-    <div>
+    <div className="home-container">
+      <div className="shape-animation"></div>
       <h2>Welcome to our ecommerce website</h2>
     </div>
   )
